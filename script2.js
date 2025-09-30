2d029b4d9df108a2ae93350d3f11d63ee0fd4b16
ciro gomemes <vkaio2022@gmail.com>
2025-09-30 12:09:29 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+//arrays -> vetores,matrizes
+//estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ['maçã', 'banana', 'laranja']; //array de tamanho 3
+console.log(frutas);
+console.log(frutas[5]);
+
+let numeros = new Array(2,8,9,10)
+console.log(numeros)
+
+
+let arrayvazio = [];
+console.log(arrayvazio);
+arrayvazio[0] = 'vazio1';
+console.log(arrayvazio);
+
+let dados= [10,50,30];
+console.log(dados.length);
